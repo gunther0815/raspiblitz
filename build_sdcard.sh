cd4a0a19aebc8bd54d40665a5c4860da10dad359
gunther0815 <108099690+gunther0815@users.noreply.github.com>
2023-06-25 02:02:37 +0200

    Update build_sdcard.sh

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -353,10 +353,10 @@ apt autoremove -y
 
 echo -e "\n*** Python DEFAULT libs & dependencies ***"
 
-if [ -f "/usr/bin/python3.9" ]; then
-  # use python 3.9 if available
-  update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
-  echo "python calls python3.9"
+if [ -f "/usr/bin/python3.11" ]; then
+  # use python 3.11 if available
+  update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
+  echo "python calls python3.11"
 elif [ -f "/usr/bin/python3.10" ]; then
   # use python 3.10 if available
   update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
